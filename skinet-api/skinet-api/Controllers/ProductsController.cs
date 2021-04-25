bbb834cc2fe1aaa10b4069d8327fe63df0009150
Jaime Tasca Junior <jaime.tasca.jr@gmail.com>
2021-04-25 02:06:30 -0300

    creation of the data, infrastructure, core projects

---
+++ skinet-api/skinet-api/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace skinet_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            List<Product> products = await _storeContext.Products.ToListAsync();
+            return Ok(products);
+        }
+            
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            Product product = await _storeContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+            return product;
+        }
+    }
+}
--- skinet-api/skinet-api/Startup.cs
+++ skinet-api/skinet-api/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,7 @@ namespace skinet_api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "skinet_api", Version = "v1" });
             });
+            services.AddDbContext<StoreContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
